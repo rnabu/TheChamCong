d2171f4c54162857a2aa11da4fb5e72972cca7b0
rnabu <77094042+rnabu@users.noreply.github.com>
2026-07-10 18:03:48 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,36 @@
+importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+apiKey:"AIzaSyCxGeOoiqcG2R1gmA5iWK9XjpcF3K0jGOs",
+authDomain:"thechamcong-dcd6e.firebaseapp.com",
+databaseURL:"https://thechamcong-dcd6e-default-rtdb.asia-southeast1.firebasedatabase.app",
+projectId:"thechamcong-dcd6e",
+storageBucket:"thechamcong-dcd6e.firebasestorage.app",
+messagingSenderId:"141017912990",
+appId:"1:141017912990:web:79ed5594b2f7ed3e624d8f"
+});
+
+const messaging = firebase.messaging();
+
 const CACHE_NAME = "chamcong-v1";
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll([
-                "/",
-                "/index.html"
-            ]);
-        })
-    );
+event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(["./","./index.html"])));
+self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
+
+messaging.onBackgroundMessage(payload => {
+self.registration.showNotification(payload.notification.title,{
+body:payload.notification.body,
+icon:"icon-192.png"
+});
 });
