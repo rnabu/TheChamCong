61dc29d944bb8b84b136f0cbcf8522e5d606985e
rnabu <77094042+rnabu@users.noreply.github.com>
2026-07-10 17:01:20 +0900

    Add Firebase messaging service worker
    test code

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+apiKey:"AIzaSyCxGeOoiqcG2R1gmA5iWK9XjpcF3K0jGOs",
+authDomain:"thechamcong-dcd6e.firebaseapp.com",
+databaseURL:"https://thechamcong-dcd6e-default-rtdb.asia-southeast1.firebasedatabase.app",
+projectId:"thechamcong-dcd6e",
+storageBucket:"thechamcong-dcd6e.firebasestorage.app",
+messagingSenderId:"141017912990",
+appId:"1:141017912990:web:79ed5594b2f7ed3e624d8f"
+});
+
+const messaging=firebase.messaging();
