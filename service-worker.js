451052339949c288e4dd03a9f4bbb69032c8687e
rnabu <77094042+rnabu@users.noreply.github.com>
2026-07-14 09:03:09 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
+console.log("Service Worker loaded");
+
 importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js");
 
